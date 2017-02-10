8456a09658e082237177280983615d9c30203838
Jimi Friis <jimi.friis@agero.se>
2017-02-10 11:31:26 +0100

    "UI - lägga till vyer för detaljer , redigering och skapa ny"  är klar, därmed är del 3.1 klar, nästa steg är implementationen av backend del 3.2 "3.2 Kommunikation mellan Controller och remote Web API",
    "UI - lägga till vyer för detaljer , redigering och skapa ny" = tidigare "(UI - lägga till detalj- och redigeringsvy)"

--- Profil.ContentAdmin/Startup.cs
+++ Profil.ContentAdmin/Startup.cs
@@ -1,14 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 
 namespace Profil.ContentAdmin
