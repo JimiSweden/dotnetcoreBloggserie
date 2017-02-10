18017877e9a78a36aab991d36a3594d20a896f60
Jimi Friis <jimi.friis@agero.se>
2017-02-10 11:31:26 +0100

    "UI - lägga till vyer för detaljer , redigering och skapa ny"  är klar, därmed är del 3.1 klar, nästa steg är implementationen av backend del 3.2 "3.2 Kommunikation mellan Controller och remote Web API",
    "UI - lägga till vyer för detaljer , redigering och skapa ny" = tidigare "(UI - lägga till detalj- och redigeringsvy)"

--- Profil.ContentAdmin/Controllers/DashboardController.cs
+++ Profil.ContentAdmin/Controllers/DashboardController.cs
@@ -17,6 +17,39 @@ namespace Profil.ContentAdmin.Controllers
             return View(model);
         }
 
+
+        public IActionResult Details(Guid id)
+        {
+            var model = new ConsultantProfileViewModel
+            {
+                FirstName = "Jimi",
+                LastName = "Friis",
+                Id = new Guid("00000000-0000-0000-0000-000000000001"),
+                Description = $"Affärsintresserad Prestigelös Agil Utvecklare med fokus på Rätt. Helikopterperspektiv – Har lätt för att överblicka och förstå hur processer, system och arbetsflöden hänger ihop.  Professionell – Tar ansvar och initiativ, frågar och vill veta varför innan vi gör.  Resultatinriktad – Drivs av att leverera bra lösningar på riktiga och viktiga problem, Rätt sak i Rätt tid ger Rätt kundnytta. Lagspelare – Trivs bäst i team och kan om det behövs ta ledande roller. ”Det optimala utvecklingsteamet enligt mig är tvärfunktionellt och agilt med mandat till förändringar och snabb tillgång till affärens områdesexperter” Som person är Jimi professionell, ödmjuk, serviceinriktad, social, ärlig, glad och lyhörd, men för den delen inte blyg i att göra sina åsikter hörda. Arbetsmetoder som TDD, BDD och DDD ligger Jimi varmt om hjärtat . Högskolestudier inom både företagsekonomi och datavetenskap, en IT-bakgrund inom serverdrift och nätverk samt erfarenhet från diverse olika yrkesområden utanför IT-världen och ett organisatoriskt sinne underlättar förståelsen för och anpassningen till olika typer av verksamheter."
+            };
+            return View(model);
+        }
+
+
+        public IActionResult Edit(Guid id)
+        {
+            var model = new ConsultantProfileViewModel
+            {
+                FirstName = "Jimi",
+                LastName = "Friis",
+                Id = new Guid("00000000-0000-0000-0000-000000000001"),
+                Description = $"Affärsintresserad Prestigelös Agil Utvecklare med fokus på Rätt. Helikopterperspektiv – Har lätt för att överblicka och förstå hur processer, system och arbetsflöden hänger ihop.  Professionell – Tar ansvar och initiativ, frågar och vill veta varför innan vi gör.  Resultatinriktad – Drivs av att leverera bra lösningar på riktiga och viktiga problem, Rätt sak i Rätt tid ger Rätt kundnytta. Lagspelare – Trivs bäst i team och kan om det behövs ta ledande roller. ”Det optimala utvecklingsteamet enligt mig är tvärfunktionellt och agilt med mandat till förändringar och snabb tillgång till affärens områdesexperter” Som person är Jimi professionell, ödmjuk, serviceinriktad, social, ärlig, glad och lyhörd, men för den delen inte blyg i att göra sina åsikter hörda. Arbetsmetoder som TDD, BDD och DDD ligger Jimi varmt om hjärtat . Högskolestudier inom både företagsekonomi och datavetenskap, en IT-bakgrund inom serverdrift och nätverk samt erfarenhet från diverse olika yrkesområden utanför IT-världen och ett organisatoriskt sinne underlättar förståelsen för och anpassningen till olika typer av verksamheter."
+            };
+            return View(model);
+        }
+
+
+        public IActionResult Create()
+        {
+            return View(new ConsultantProfileViewModel());
+        }
+
+
         private ConsultantProfilesViewModel GetDummyProfiles()
         {
             return new ConsultantProfilesViewModel
---
+++ Profil.ContentAdmin/ViewModels/ConsultantProfileViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Profil.ContentAdmin.ViewModels
+{
+    public class ConsultantProfileViewModel
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Description { get; set; }
+    }
+}
